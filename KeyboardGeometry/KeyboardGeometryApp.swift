8a7679158df8fbf30cd0f600c57123565d536052
Jake Smolowe <jakesmolowe@gmail.com>
2024-07-02 15:11:51 -0700

    Initial Commit

---
+++ KeyboardGeometry/KeyboardGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardGeometryApp.swift
+//  KeyboardGeometry
+//
+//  Created by Jake Smolowe on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
